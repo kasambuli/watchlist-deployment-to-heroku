90538b2aa11f8cb8ffc048f3e17a14e4b1a8a28d
Burence1 <burensomondi@gmail.com>
2021-04-14 11:25:57 +0300

    add styling to the application

--- app/views.py
+++ app/views.py
@@ -19,14 +19,6 @@ def index():
     title = 'Home - Welcome to The best Movie Review Website Online'
     return render_template('index.html', title=title, popular=popular_movies)
 
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
-    '''
-    View movie page function that returns the movie details page and its data
-    '''
-
-    return render_template('movie.html', id = movie_id)
-
 
 @app.route('/movie/<int:id>')
 def movie(id):
