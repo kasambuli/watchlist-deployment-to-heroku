00485175ffb6f13f92891c40c0d711451feeae49
Burence1 <burensomondi@gmail.com>
2021-04-15 18:15:52 +0300

    change flask folder structure

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
+import os
+
 class Config:
 
   '''
   General configurations parent class
   '''
   MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
+  MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+  SECRET_KEY = os.environ.get('SECRET_KEY')
   
 
 class ProdConfig(Config):
@@ -23,3 +27,8 @@ class DevConfig(Config):
       Config:The parent configuration class with General configuration settings
   '''
   DEBUG = True
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY='8aa91a6653dfe774a8f67145f627a58e'
+export SECRET_KEY='7e4a4bf4-f874-40ee-830a-ece97953f351'
+
+python3.6 manage.py server
