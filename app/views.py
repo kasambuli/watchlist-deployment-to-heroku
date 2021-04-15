cf3dac9922b5c38a0802b4dec27bfa67ce108071
Burence1 <burensomondi@gmail.com>
2021-04-15 08:51:50 +0300

    fix bug in search logic

--- app/views.py
+++ app/views.py
@@ -20,10 +20,10 @@ def index():
 
     title = 'Home - Welcome to The best Movie Review Website Online'
 
-    search_movie = request.args.get('Movie_query')
+    search_movie = request.args.get('movie_query')
 
     if search_movie:
-        return redirect(url_for('search',movie_name=search_movie))
+        return redirect(url_for('search',movie_name = search_movie))
     else:
         return render_template('index.html', title=title, popular=popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie)
 
