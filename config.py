00de0635598a4d23fed84fc8c59a4781a5375b8a
Burence1 <burensomondi@gmail.com>
2021-04-22 21:15:20 +0300

    fix bug

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ class ProdConfig(Config):
   Args:
       Config: The parent configuration class with General configuration settings
   '''
-  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL?sslmode=require")
 
 class DevConfig(Config):
   '''
